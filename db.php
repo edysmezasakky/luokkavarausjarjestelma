468c120b1cd300485476d177e62e2d9d011b7ebf
Edys Meza <edys.meza@edu.sakky.fi>
2026-02-12 19:05:31 +0200

    asennetaan tietokannan muotojat

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = 'db';
-$db = 'db';
-$user = 'db';
-$pass = 'db';
+$host = '127.0.0.1';
+$db = 'luokkavaraus';
+$user = 'root';
+$pass = '';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host; dbname=$db;charset=$charset";
 $options = [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
